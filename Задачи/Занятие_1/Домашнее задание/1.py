1e30b2c8a9e8d9b8a3411366a1528cb1e81a11e5
makcuto4ka <153239389+makcuto4ka@users.noreply.github.com>
2024-09-07 19:31:47 +0300

    Update 1.py

--- Задачи/Занятие_1/Домашнее задание/1.py
+++ Задачи/Занятие_1/Домашнее задание/1.py
@@ -1,5 +1,6 @@
 import math 
-x=int(input('Введите x:'))
-t=int(input('Введите t:'))
-z= round((9*math.pi*t+10*math.cos(x))/(math.pow(t,0.5)- abs(math.sin(t)))*math.e**x,2)
-print(z)
+r=int(input('Введите R:'))
+p=round(2*math.pi*r,2)
+s= round(math.pi*r**2,2)
+print('Длина окружности с радиусом',r,'см равна',p)
+print('Площадь окружности с радиусом',r,'см равна',s)
