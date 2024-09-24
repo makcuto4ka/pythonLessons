0bc8dbb6e453ca33a94d19c6e20bcad9b88ca3a3
makcuto4ka <153239389+makcuto4ka@users.noreply.github.com>
2024-09-24 15:29:42 +0300

    Update 2.py

--- Задачи/Занятие_3/Домашнее задание/2.py
+++ Задачи/Занятие_3/Домашнее задание/2.py
@@ -1,3 +1,6 @@
-for i in range(1,10):
-    for j in range(1,10):
-        print(i,'*',j,'=',i*j)
+m=''
+for i in range(1,11):
+    for j in range(1,11):
+      m+=str(j*i)+' '*(3-len(str(j*i)))
+    print(m)
+    m=''
