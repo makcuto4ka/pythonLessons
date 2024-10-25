9ae3087b79b3263e4ac9c2e3ac386f166cea43ba
makcuto4ka <153239389+makcuto4ka@users.noreply.github.com>
2024-10-25 22:32:27 +0000

    Занятие_7

--- Занятие_7.py
+++ Занятие_7.py
@@ -1,67 +1,67 @@
-# #1
-# print('\n#1')
+#1
+print('\n#1')
 
-# def read_last(lines, file):
-#   abstract_file = open('article.txt', 'r', encoding='utf-8')
-#   for n in range(lines):
-#     print(abstract_file.readline(),end='')
+def read_last(lines, file):
+  abstract_file = open('article.txt', 'r', encoding='utf-8')
+  for n in range(lines):
+    print(abstract_file.readline(),end='')
 
-# n = int(input('Input int positive number: '))
-# file = open('article.txt', 'r', encoding='utf-8')
-# rows_number = len(file.readlines()) 
-# if n > 0 and n < rows_number:
-#   read_last(n, file)
+n = int(input('Input int positive number: '))
+file = open('article.txt', 'r', encoding='utf-8')
+rows_number = len(file.readlines()) 
+if n > 0 and n < rows_number:
+  read_last(n, file)
 
-# #2
-# import os
+#2
+import os
  
-# def print_docs(directory):
-#   all_files = os.walk(directory)
-#   for catalog in all_files:
-#     print(f'Папка {catalog[0]} содержит:')
-#     print(f'Директории: {", ".join([folder for folder in catalog[1]])}')
-#     print(f'Файлы: {", ".join([file for file in catalog[2]])}')
-#     print('-' * 40)
+def print_docs(directory):
+  all_files = os.walk(directory)
+  for catalog in all_files:
+    print(f'Папка {catalog[0]} содержит:')
+    print(f'Директории: {", ".join([folder for folder in catalog[1]])}')
+    print(f'Файлы: {", ".join([file for file in catalog[2]])}')
+    print('-' * 40)
  
  
-# print_docs('C:/Users/User/Downloads/2')
+print_docs('C:/Users/User/Downloads/2')
 
 
-# #3
-# print('\n#3')
-# from functools import reduce
-# def longest_words(file):
-#     rows = file.readlines()
-#     words= reduce(lambda x,y:  x[:-1]+' '+y, rows).split()
-#     max_len_word= max(map(len, words))
-#     return list(filter(lambda x: len(x)==max_len_word, words))
-# file = open('article.txt', 'r', encoding='utf-8')
-# print(longest_words(file))
+#3
+print('\n#3')
+from functools import reduce
+def longest_words(file):
+    rows = file.readlines()
+    words= reduce(lambda x,y:  x[:-1]+' '+y, rows).split()
+    max_len_word= max(map(len, words))
+    return list(filter(lambda x: len(x)==max_len_word, words))
+file = open('article.txt', 'r', encoding='utf-8')
+print(longest_words(file))
 
-# #4
-# print('\n#4')
-# name= input('Введите имя файла: ')+'.txt'
-# file = open(name, "w")
-# s=input('Введите строку (для завершения нажмите ENTER): ')+'\n'
-# while len(s)>1:
-#     file.write(s)
-#     s=input('Введите строку (для завершения нажмите ENTER): ')+'\n'
-# print('Работа с файлом окончена')
-# #5
-# print('\n#5')
+#4
+print('\n#4')
+name= input('Введите имя файла: ')+'.txt'
+file = open(name, "w")
+s=input('Введите строку (для завершения нажмите ENTER): ')+'\n'
+while len(s)>1:
+    file.write(s)
+    s=input('Введите строку (для завершения нажмите ENTER): ')+'\n'
+print('Работа с файлом окончена')
+#5
+print('\n#5')
 
-# import csv
-# import datetime
-# import time
+import csv
+import datetime
+import time
  
-# file=open('rows_300.csv', 'w', encoding='utf-8')
-# writer = csv.writer(file)
-# writer.writerow(['№', 'Секунда ', 'Микросекунда'])
-# for i in range(1, 301):
-#     writer.writerow([i, datetime.datetime.now().second, datetime.datetime.now().microsecond])
-#     time.sleep(0.01)
+file=open('rows_300.csv', 'w', encoding='utf-8')
+writer = csv.writer(file)
+writer.writerow(['№', 'Секунда ', 'Микросекунда'])
+for i in range(1, 301):
+    writer.writerow([i, datetime.datetime.now().second, datetime.datetime.now().microsecond])
+    time.sleep(0.01)
         
-# #6
+#6
 print('\n#6')
 
 from random import randint
