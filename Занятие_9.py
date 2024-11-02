5c3bde36e42a9b1f003dec87db37c6b15af0c064
makcuto4ka <153239389+makcuto4ka@users.noreply.github.com>
2024-11-02 14:05:59 +0000

    9

--- Занятие_9.py
+++ Занятие_9.py
@@ -1,23 +1,27 @@
-#1
+# 1
+from matplotlib import pyplot as plt
+import os
+import numpy as np
 print('\n#1')
 
-import numpy as np
-import os
-from matplotlib import pyplot as plt
 
 dir = os.getcwd()
 x = np.linspace(-2, 2, 1000)
 
 plt.rcParams["figure.figsize"] = [5, 5]
 plt.rcParams["figure.autolayout"] = True
-def f(x,a,b):
-    y= (x**b+a**b)/x**b
-    y[y > 20]=np.nan
-    y[y < -20]=np.nan
+
+
+def f(x, a, b):
+    y = (x**b+a**b)/x**b
+    y[y > 20] = np.nan
+    y[y < -20] = np.nan
     return y
-plt.plot(x, f(x,a=1,b=1), label='a=1,b=1', color='red')  
-plt.plot(x, f(x,a=2,b=1), label='a=2,b=1', color='blue')
-plt.plot(x, f(x,a=1,b=2), label='a=1,b=2', color='green') 
+
+
+plt.plot(x, f(x, a=1, b=1), label='a=1,b=1', color='red')
+plt.plot(x, f(x, a=2, b=1), label='a=2,b=1', color='blue')
+plt.plot(x, f(x, a=1, b=2), label='a=1,b=2', color='green')
 plt.xlabel('Ось X')
 plt.ylabel('Ось Y')
 plt.title('f(x)=(x^b+a^b)/x^b')
@@ -26,71 +30,73 @@ plt.grid()
 plt.show()
 plt.savefig(dir + '/Занятие_9_1.svg')
 
-#2
+# 2
 print('\n#2')
 
-import numpy as np
-import os
-from matplotlib import pyplot as plt
 
 dir = os.getcwd()
 x = np.linspace(0, 5, 1000)
 
 plt.rcParams["figure.figsize"] = [5, 5]
 plt.rcParams["figure.autolayout"] = True
-def f(x,a,b):
-    y= (x**b+a**b)/x**b
-    y[y > 20]=np.nan
-    y[y < -20]=np.nan
+
+
+def f(x, a, b):
+    y = (x**b+a**b)/x**b
+    y[y > 20] = np.nan
+    y[y < -20] = np.nan
     return y
-plt.plot(x, f(x,a=1,b=1), label='a=1,b=1', color='red')  
-plt.plot(x, f(x,a=2,b=1), label='a=2,b=1', color='blue')
-plt.plot(x, f(x,a=1,b=2), label='a=1,b=2', color='green') 
+
+
+plt.plot(x, f(x, a=1, b=1), label='a=1,b=1', color='red')
+plt.plot(x, f(x, a=2, b=1), label='a=2,b=1', color='blue')
+plt.plot(x, f(x, a=1, b=2), label='a=1,b=2', color='green')
 plt.xlabel('Ось X')
 plt.ylabel('Ось Y')
 plt.title('f(x)=(x^b+a^b)/x^b')
 plt.legend()
 plt.grid()
 
-plt.axes([0.35, 0.60, 0.25, 0.25]) 
+plt.axes([0.35, 0.60, 0.25, 0.25])
 x = np.linspace(0, 0.5, 1000)
 plt.grid()
 plt.title('Для малых x')
-plt.plot(x, f(x,a=1,b=1), label='a=1,b=1', color='red')  
-plt.plot(x, f(x,a=2,b=1), label='a=2,b=1', color='blue')
-plt.plot(x, f(x,a=1,b=2), label='a=1,b=2', color='green')  
+plt.plot(x, f(x, a=1, b=1), label='a=1,b=1', color='red')
+plt.plot(x, f(x, a=2, b=1), label='a=2,b=1', color='blue')
+plt.plot(x, f(x, a=1, b=2), label='a=1,b=2', color='green')
 
-plt.axes([0.70, 0.30, 0.25, 0.25]) 
+plt.axes([0.70, 0.30, 0.25, 0.25])
 x = np.linspace(10, 20, 1000)
 plt.grid()
 plt.title('Для больших x')
-plt.plot(x, f(x,a=1,b=1), label='a=1,b=1', color='red')  
-plt.plot(x, f(x,a=2,b=1), label='a=2,b=1', color='blue')
-plt.plot(x, f(x,a=1,b=2), label='a=1,b=2', color='green')  
+plt.plot(x, f(x, a=1, b=1), label='a=1,b=1', color='red')
+plt.plot(x, f(x, a=2, b=1), label='a=2,b=1', color='blue')
+plt.plot(x, f(x, a=1, b=2), label='a=1,b=2', color='green')
 
 plt.show()
 plt.savefig(dir + '/Занятие_9_2.svg')
 
-#3
+# 3
 print('\n#3')
 
-import numpy as np
-import os
-from matplotlib import pyplot as plt
 
 dir = os.getcwd()
 x = np.linspace(-5, 0, 1000)
 
 plt.rcParams["figure.figsize"] = [5, 5]
 plt.rcParams["figure.autolayout"] = True
-def f(x,a,b):
-    y= (x**b+a**b)/x**b
-    y[y > 20]=np.nan
-    y[y < -20]=np.nan
+
+
+def f(x, a, b):
+    y = (x**b+a**b)/x**b
+    y[y > 20] = np.nan
+    y[y < -20] = np.nan
     return y
-plt.plot(x, f(x,a=1,b=1), label='a=1,b=1', color='red')  
-plt.plot(x, f(x,a=2,b=1), label='a=2,b=1', color='blue')
-plt.plot(x, f(x,a=1,b=2), label='a=1,b=2', color='green') 
+
+
+plt.plot(x, f(x, a=1, b=1), label='a=1,b=1', color='red')
+plt.plot(x, f(x, a=2, b=1), label='a=2,b=1', color='blue')
+plt.plot(x, f(x, a=1, b=2), label='a=1,b=2', color='green')
 plt.plot(x, [0]*len(x), label='x=0', color='black')
 plt.xlabel('Ось X')
 plt.ylabel('Ось Y')
@@ -98,28 +104,29 @@ plt.title('f(x)=(x^b+a^b)/x^b')
 plt.legend()
 plt.grid()
 
-plt.axes([0.21, 0.17, 0.50, 0.25]) 
-def f(x,a,b):
-    y= (x**b+a**b)/x**b
-    y[y > 3]=np.nan
-    y[y < -3]=np.nan
+plt.axes([0.21, 0.17, 0.50, 0.25])
+
+
+def f(x, a, b):
+    y = (x**b+a**b)/x**b
+    y[y > 3] = np.nan
+    y[y < -3] = np.nan
     return y
+
+
 x = np.linspace(-5, 0, 1000)
 plt.grid()
-plt.plot(x, f(x,a=1,b=1), label='a=1,b=1', color='red')  
-plt.plot(x, f(x,a=2,b=1), label='a=2,b=1', color='blue')
-plt.plot(x, f(x,a=1,b=2), label='a=1,b=2', color='green')  
+plt.plot(x, f(x, a=1, b=1), label='a=1,b=1', color='red')
+plt.plot(x, f(x, a=2, b=1), label='a=2,b=1', color='blue')
+plt.plot(x, f(x, a=1, b=2), label='a=1,b=2', color='green')
 plt.plot(x, [0]*len(x), label='x=0', color='black')
 
 plt.show()
 plt.savefig(dir + '/Занятие_9_3.svg')
 
-#4
+# 4
 print('\n#4')
 
-import numpy as np
-import os
-from matplotlib import pyplot as plt
 
 dir = os.getcwd()
 x = np.linspace(-5, 5, 1000)
@@ -127,28 +134,31 @@ x = np.linspace(-5, 5, 1000)
 plt.rcParams["figure.figsize"] = [5, 5]
 plt.rcParams["figure.autolayout"] = True
 
-def f(x,a,b):
-    y= (x**b+a**b)/x**b
-    y[y > 20]=np.nan
-    y[y < -20]=np.nan
+
+def f(x, a, b):
+    y = (x**b+a**b)/x**b
+    y[y > 20] = np.nan
+    y[y < -20] = np.nan
     return y
 
 
-plt.plot(x, f(x,a=1,b=0.5), label='a=1,b=0.5', color='red')  
-plt.plot(x, f(x,a=1,b=-0.5), label='a=1,b=-0.5', color='blue')
-plt.plot(x, f(x,a=1,b=-1.5), label='a=1,b=-1.5', color='green') 
+plt.plot(x, f(x, a=1, b=0.5), label='a=1,b=0.5', color='red')
+plt.plot(x, f(x, a=1, b=-0.5), label='a=1,b=-0.5', color='blue')
+plt.plot(x, f(x, a=1, b=-1.5), label='a=1,b=-1.5', color='green')
 plt.xlabel('Ось X')
 plt.ylabel('Ось Y')
 plt.title('f(x)=(x^b+a^b)/x^b')
 plt.legend()
 plt.grid()
 x = np.linspace(-1, 2, 1000)
-def example_plot(ax,x,a,b1,b2, array_b,k, fontsize=12, hide_labels=False):
-    ax.plot(x, f(x,a,b1),label=f'a={a},b={b1}',color='red') 
-    ax.plot(x, f(x,a,b2),label=f'a={a},b={b1}',color='blue')
+
+
+def example_plot(ax, x, a, b1, b2, array_b, k, fontsize=12, hide_labels=False):
+    ax.plot(x, f(x, a, b1), label=f'a={a},b={b1}', color='red')
+    ax.plot(x, f(x, a, b2), label=f'a={a},b={b1}', color='blue')
     ax.grid()
-    ax.plot(x, f(x,a,array_b[k][0]), label=f'a={a},b={array_b[k][0]}',color='green')
-    ax.plot(x, f(x,a,array_b[k][1]), label=f'a={a},b={array_b[k][0]}',color='indianred')
+    ax.plot(x, f(x, a, array_b[k][0]), label=f'a={a},b={array_b[k][0]}', color='green')
+    ax.plot(x, f(x, a, array_b[k][1]), label=f'a={a},b={array_b[k][0]}', color='indianred')
     ax.legend()
     ax.locator_params(nbins=3)
     if hide_labels:
@@ -157,15 +167,17 @@ def example_plot(ax,x,a,b1,b2, array_b,k, fontsize=12, hide_labels=False):
     else:
         ax.set_xlabel('Ось X', fontsize=fontsize)
         ax.set_ylabel('Ось Y', fontsize=fontsize)
-fig, axs = plt.subplots(1,3,figsize=(15, 6))
-a=1
-b1=0
-b2=-1
-array_b=[[0.5,0.8],[-0.5,-0.8],[-1.5,-2.5]]
-k=0
-for ax in axs.flat:
-    example_plot(ax,x,a,b1,b2,array_b,k)
-    k+=1
 
-plt.show()
-plt.savefig(dir + '/Занятие_9_4.svg')
+
+fig, axs = plt.subplots(1, 3, figsize=(15, 6))
+a = 1
+b1 = 0
+b2 = -1
+array_b = [[0.5, 0.8], [-0.5, -0.8], [-1.5, -2.5]]
+k = 0
+for ax in axs.flat:
+    example_plot(ax, x, a, b1, b2, array_b, k)
+    k += 1
+
+# plt.show()
+plt.savefig(dir + '/Занятие_9_4.png', dpi=300)
