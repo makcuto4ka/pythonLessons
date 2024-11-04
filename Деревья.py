050dad4db2ded8940b6ab32664f815affe2780b1
makcuto4ka <153239389+makcuto4ka@users.noreply.github.com>
2024-11-04 00:42:59 +0000

    tree

--- Деревья.py
+++ Деревья.py
@@ -1,5 +1,5 @@
 import json
-from matplotlib import pyplot as plt
+from PIL import Image, ImageDraw, ImageFont
 
 
 def help():
@@ -8,17 +8,24 @@ def help():
     print('  del - удаление листочка или ветки')
     print('  save - сохранение дерева в файл')
     print('  open - открытие дерева из файла')
+    print('  show - нарисовать дерево')
+    print('  depth - узнать максимальную глубину дерева')
     print('Для завершения программы нажмите Enter')
 
 def save_tree(tree):
-    name_file=input('Введите название файла: ')
-    with open(f'{name_file}.txt', 'w') as fw:
-        return json.dump(tree, fw)
-
+    try:
+        name_file=input('Введите название файла: ')
+        with open(f'{name_file}.txt', 'w') as fw:
+            return json.dump(tree, fw)
+    except FileNotFoundError:
+        print('Ошибка! Вы ввели недопустимый символ для имени файла.')
 def open_tree():
     name_file=input('Введите название файла: ')
-    with open(f'{name_file}.txt', 'r') as fr:
-        return  json.load(fr)
+    try:
+        with open(f'{name_file}.txt', 'r') as fr:
+            return  json.load(fr)
+    except FileNotFoundError:
+        print('Ошибка! Файл не найден.')
     
 def new_child(tree,navi):
     copy_tree=[tree]
@@ -84,19 +91,72 @@ def max_depth(tree):
         return 1 + max(max_depth(i) for i in tree) 
     else:
         return 0
+
+def sort_tree(tree):
+    new_tree=[]
+    last_tree=tree
+    k=0
+    index=[0]
+    while len(list(filter(lambda x: type(x)!=list and x!=None, last_tree)))>0:
+        new_tree.append([None]*2**k)
+        list_str=list(filter(lambda x: type(x)==str, last_tree))
+        k3=0
+        for i in index:
+            new_tree[k][i]=(list_str[k3])
+            k3+=1
+        index=[]
+        k2=0
+        for i in last_tree:
+            if type(i)==list:
+                index.append(k2)
+            elif type(i)==str:
+                k2-=1
+            k2+=1
+        last_tree=list(filter(lambda x: type(x)==list, last_tree))
+        arr=[]
+        for i in last_tree:
+            arr+=i
+        last_tree=arr
+        k+=1
+    return new_tree
+
+def show_tree(tree):  
+    s_tree=sort_tree(tree)
+    k=len(s_tree)
+    h=k*50
+    w=k*150
+    img = Image.new('RGB', [w, h], color='white')
+    draw = ImageDraw.Draw(img)
+    size=20
+    font = ImageFont.load_default(size)
+    ki=0
+    for i in range(len(s_tree)):
+        kj=0
+        kj2=0
+        for j in range(len(s_tree[i])):
+            if s_tree[i][j] != None:
+                draw.text((w*(kj+1)//(len(s_tree[i])+1)-len(s_tree[i][j])*size//4, ki*50), s_tree[i][j], 'black',font=font) 
+            if i+1!=len(s_tree):
+                if s_tree[i][j] != None and s_tree[i+1][j] != None:
+                    img1 = ImageDraw.Draw(img)  
+                    img1.line([(w*(kj+1)//(len(s_tree[i])+1), ki*50+20),(w*(kj2+1)//(2**(ki+1)+1),ki*50+50)], fill ='black', width = 4)
+                if s_tree[i][j] != None and s_tree[i+1][j+1] != None:
+                    img1 = ImageDraw.Draw(img)  
+                    img1.line([(w*(kj+1)//(len(s_tree[i])+1), ki*50+20),(w*(kj2+2)//(2**(ki+1)+1),ki*50+50)], fill ='black', width = 4)
+            kj+=1
+            kj2+=2
+        ki+=1
+    img.show()
+    action=input('Для сохранения картинки дерева введите save: ').lower()
+    if action=='save':
+        name_file=input('Введите название файла: ')
+        img.save(f'{name_file}.png') 
     
-def show_tree(tree):
-    k=max_depth(tree)    
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    fig.suptitle('Дерево', fontsize=14, fontweight='bold')
-    ax.axis([0, k*3, 0, k*10])
-    ax.annotate('annotate', xy=(5, 10), xytext=(4, 10),arrowprops=dict(facecolor='black', shrink=0.05))
-    plt.show()
+
 tree=[None,None,None]
 
 
-action=input('Введите действие (для помощи введите - help): ') 
+action=input('Введите действие (для помощи введите - help): ').lower()
 while action!='':
     if action=='new':
         navi=input('Введите путь к нужному листочку (для изменения корня нажмите Enter): ').upper() 
@@ -107,14 +167,14 @@ while action!='':
     elif action=='save': 
         save_tree(tree)
     elif action=='open':
-        open_tree()
+        tree=open_tree()
     elif action=='help':
         help()
+    elif action=='show':
+        show_tree(tree)
+    elif action=='depth':
+        print(max_depth(tree))
     else:
         print('Ошибка! Команды не существует.')
     print(tree)
-    action=input('Введите действие (для помощи введите - help): ') 
-    
-        
-        
-        
+    action=input('Введите действие (для помощи введите - help): ').lower()
