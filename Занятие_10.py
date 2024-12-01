089c6c14ccc5bf280174740101b4d6136b7df381
makcuto4ka <you@example.com>
2024-12-02 02:07:41 +0300

    10

--- Занятие_10.py
+++ Занятие_10.py
@@ -19,3 +19,22 @@ print( df[df != df.loc[df['fruits_1'].isin(df['fruits_2']), 'fruits_1'].values].
 #3
 print('\n#3')
 
+import pandas as pd
+from matplotlib import pyplot as plt
+
+sr= pd.Series(["orange","orange","orange","orange","orange", "tangerine", "banana", "tangerine", "banana", "tangerine", "banana", "tangerine", "tangerine", "tangerine", "tangerine"])
+print(sr)
+
+plt.bar(sr.value_counts().index, sr.value_counts().values)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+#4
+print('\n#4')
+
+import pandas as pd
+
+df= pd.DataFrame({
+    'fruits': ["apple", "pear", "orange", "tangerine", "banana"],
+    'fruits_2': [ "tangerine", "banana", "kiwi", "peach", "plum"]})
+print(df)
