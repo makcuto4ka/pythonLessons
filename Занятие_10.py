c33569f02525f2037cb53ab9cf1e5c13c5254fe7
makcuto4ka <you@example.com>
2024-12-06 01:09:48 +0300

    11

--- Занятие_10.py
+++ Занятие_10.py
@@ -34,7 +34,45 @@ print('\n#4')
 
 import pandas as pd
 
-df= pd.DataFrame({
+df_fruits= pd.DataFrame({
     'fruits': ["apple", "pear", "orange", "tangerine", "banana"],
-    'fruits_2': [ "tangerine", "banana", "kiwi", "peach", "plum"]})
-print(df)
+    'vegetables': [ "onion", "garlic", "beetroot", "carrot", "cabbage"]})
+print('fruits')
+print(df_fruits)
+df_vegetables= pd.DataFrame({
+    'vegetables': ["cucumber", "tomato", "zucchini", "eggplant", "potato"],
+    'fruits': [ "tangerine", "banana", "kiwi", "peach", "plum"]})
+print('vegetables')
+print(df_vegetables)
+df_berries= pd.DataFrame({
+    'berries': [ "strawberry", "raspberry", "blueberry", "blueberry", "currant", "gooseberry", "cherry", "cherry", "grape", "watermelon"]})
+df_berries.index.name = 'Number'
+print('berries')
+print(df_berries)
+df_fruits_vegetables= pd.concat([df_fruits,df_vegetables]).reset_index(drop=True)
+df_fruits_vegetables.index.name = 'Number'
+print('fruits_vegetables')
+print( df_fruits_vegetables)
+df_fruits_vegetables_berries= pd.merge(df_fruits_vegetables,df_berries,how = "outer",on='Number')
+print('fruits_vegetables_berries')
+print(df_fruits_vegetables_berries)
+
+
+#5
+print('\n#5')
+from matplotlib import pyplot as plt
+import pandas as pd
+
+df_fruits_price = pd.DataFrame({
+    'Banana':[0.34, 0.36, 0.35, 0.39, 0.36, 0.37, 0.38, 0.36, 0.42, 0.45, 0.46, 0.48, 0.46, 0.44, 0.46, 0.49, 0.49, 0.49, 0.49, 0.49, 0.5, 0.51, 0.51, 0.51, 0.5, 0.49, 0.5, 0.51, 0.61, 0.61, 0.58, 0.61, 0.6, 0.6, 0.6, 0.58, 0.57, 0.56],
+    'Apple':[0.63, 0.57, 0.64, 0.59, 0.66, 0.68, 0.77, 0.73, 0.73, 0.69, 0.72, 0.89, 0.89, 0.83, 0.8, 0.83, 0.93, 0.91, 0.94, 0.9, 0.92, 0.87, 0.95, 0.98, 1.04, 0.95, 1.07, 1.12, 1.32, 1.18, 1.22, 1.35, 1.38, 1.39, 1.35, 1.36, 1.44, 1.29] 
+}, index=[i for i in range(1980,2018)]) 
+print(list(df_fruits_price['Banana']))
+
+plt.plot(list(df_fruits_price.index),list(df_fruits_price['Banana']), label='Banana', color='green')
+plt.plot(list(df_fruits_price.index),list(df_fruits_price['Apple']), label='Apple', color='red')
+plt.xlabel('Год')
+plt.ylabel('Средняя цена за фунт (453,6 г), $')
+plt.legend()
+plt.grid()
+plt.show()
--- Занятие_11.py
+++ Занятие_11.py
@@ -1,37 +1,55 @@
-# #1
+#1
+print('\n#1')
 
-# import pandas as pd
-# file = 'NISPUF17.csv'
-# df = pd.read_csv(file, sep=',')
-# print(df)
-# children_count=df
-# df['EDUC1']= df['EDUC1'].replace([1,2,3,4],['<12','12','>12','CG'], regex= True)
+import pandas as pd
+
+df = pd.read_csv('./NISPUF17.csv', sep=',') 
 # print(df['EDUC1'].unique())
+# print(df['EDUC1'].where(df['EDUC1'] == 1).count())
 
-# #count_less_12_a= int(df['EDUC1'].where(df['EDUC1']=='<12').count())
-# #print(count_less_12_a)
+def proportion_of_education(dataframe):
 
-# data_1 = df.groupby('EDUC1')['EDUC1'].count()
-# # d = {data_1.index[0]: float(data_1[0])/children_count,
-# #      data_1.index[1]: float(data_1[1])/children_count,
-# #      data_1.index[2]: float(data_1[2])/children_count,
-# #      data_1.index[3]: float(data_1[3])/children_count,
-# #     }
-# print(data_1)
+    len_of_df = len(df)
+    less_than_12 = round(float(df['EDUC1'].where(df['EDUC1'] == 1).count() / len_of_df), 2)
+    e12 = round(float(df['EDUC1'].where(df['EDUC1'] == 2).count() / len_of_df), 2)
+    more_than_12 = round(float(df['EDUC1'].where(df['EDUC1'] == 3).count() / len_of_df), 2)
+    college = round(float(df['EDUC1'].where(df['EDUC1'] == 4).count() / len_of_df), 2)
 
-# #2
+    pror =  {"less than high school":less_than_12,
+    "high school":e12,
+    "more than high school but not college":more_than_12,
+    "college":college}
 
-# import pandas as pd
-# file = 'NISPUF17.csv'
-# def  average_influenza_doses(file):
-#     df = pd.read_csv('file', sep=',')
-#     df = df.dropna(subset='P_NUMFLU')
-#     df['CBF_01'] = df ['CBF_01'].replace([1,2], ['yes','no'] )
+    print(pror)
+
+proportion_of_education(df)
+
+
+#2
+print('\n#2')
+import pandas as pd
+
+# file = './NISPUF17.csv'
 # df = pd.read_csv(file, sep=',')
+# # df['CBF_01'] = df['CBF_01'].replace(1, 'yes', regex=True)
+# # df['CBF_01'] = df['CBF_01'].replace(2, 'no', regex=True)
+# # df['CBF_01'] = df['CBF_01'].replace(77, 'dk', regex=True)
+# # df['CBF_01'] = df['CBF_01'].replace(99, 'miss', regex=True)
 
+# bm = df.groupby('CBF_01')['P_NUMFLU'].mean()
+# bm_tuple = (round(float(bm[1]), 1), round(float(bm[2]), 1))
+# print(bm_tuple)
+
+
+
+df = pd.read_csv('./NISPUF17.csv', sep=',')
+df = df.dropna(subset=['P_NUMFLU'])
+bm = df.groupby('CBF_01')['P_NUMFLU'].mean()
+print(round(float(bm[1]), 1), round(float(bm[2]), 1))
 
 
 #3
+print('\n#3')
 
 import pandas as pd
 file = 'NISPUF17.csv'
