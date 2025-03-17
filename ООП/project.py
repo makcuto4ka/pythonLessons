81f5ec90f42b391195dbdf0ebc99c58d0022ce2f
makcuto4ka <maximus.frid@mail.ru>
2025-03-17 03:21:45 +0300

    1

---
+++ ООП/project.py
@@ -0,0 +1,50 @@
+import pygame
+from mutagen.mp3 import MP3
+import os
+import time
+
+class MP3Player:
+    def __init__(self):
+        pygame.mixer.init()
+
+
+    def load(self, file_path):
+        if not os.path.isfile(file_path):
+            raise FileNotFoundError(f"File {file_path} not found")
+        
+        self.file_path = file_path
+        pygame.mixer.music.load(file_path)
+        
+        audio = MP3(file_path)
+        self.duration = audio.info.length
+
+    def play(self):
+        pygame.mixer.music.play()
+        self.is_paused = False
+
+
+    def set_volume(self, volume):
+        pygame.mixer.music.set_volume(max(0.0, min(1.0, volume)))
+
+
+
+    def get_duration(self):
+        return self.duration
+
+
+player = MP3Player()
+player.load("/home/maksim/ВУЗ/pythonLessons/ООП/test.mp3") 
+
+
+volume1=float(input('Выберете громкость от 0 до 1: '))
+print(f"Длительность трека: {player.get_duration():.2f} сек")
+time_music=player.get_duration()
+player.set_volume(volume1)
+t= time.time()
+player.play()
+
+while time.time()-t < time_music:
+        print(f"\rПроиграно: {round(time.time()-t,2)} сек / {player.get_duration():.2f} сек", end="")
+        time.sleep(0.1)
+
+    
--- ООП/Занятие_6.py
+++ ООП/Занятие_6.py
@@ -1,23 +1,37 @@
+
+
 import math
 import matplotlib.pyplot as plt
-import numpy as np
 
-class Derivate:
-    def __init__(self, instance):
-        self.instance=None
+class Derivative:
+    def __init__(self, h=1e-5):
+        self.h = h
 
-    def __call__(self, instance,x, dx=0.00001, *args, **kwargs):
-        return (self.__fn(x + dx) - self.__fn(x-dx))/ 2*dx
     def __get__(self, instance, owner):
-        self.instance=instance
-        return self
+        if instance is None:
+            return self
+        
+        return lambda x: (instance(x + self.h) - instance(x - self.h)) / (2 * self.h)
+
 class ExponentialFunction:
-    derivative = Derivate()
-    def __init__(self,a):
-        self.__a=a
-    def __call__(self,x, *args, **kwds):
-        self.__exp=self.__a*math.exp(x)
-        return (self.__exp)
+    derivative = Derivative()  
+
+    def __init__(self, a):
+        self.a = a
+
+    def __call__(self, x):
+        return self.a * math.exp(x)
+
+    def plot(self):
+
+        x_vals = [x / 10.0 for x in range(-20, 21)]  
+        f_vals = [self(x) for x in x_vals]
+        df_vals = [self.derivative(x) for x in x_vals]
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(x_vals, f_vals, )
+        plt.plot(x_vals, df_vals,)
+        plt.show()
 
 exp_func = ExponentialFunction(a=2)
 print(exp_func(0))          # 2.0
@@ -47,53 +61,3 @@ exp_func.plot()
 
 
 
-
-
-
-
-
-import math
-import matplotlib.pyplot as plt
-import numpy as np
-
-class Derivative:
-    def __call__(self, instance, x):
-        h = 1e-5
-        return (instance(x + h) - instance(x - h)) / (2 * h)
-    
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-        return lambda x: self(instance, x)
-
-class ExponentialFunction:
-    derivative = Derivative()
-    
-    def __init__(self, a):
-        self.a = a
-    
-    def __call__(self, x):
-        return self.a * math.exp(x)
-    
-    def plot(self):
-        x_vals = np.linspace(-2, 2, 100)
-        f_vals = [self(x) for x in x_vals]
-        df_vals = [self.derivative(x) for x in x_vals]
-        
-        plt.figure()
-        plt.plot(x_vals, f_vals, label=f'$f(x) = {self.a}e^x$')
-        plt.plot(x_vals, df_vals, label=f"$f'(x) = {self.a}e^x$", linestyle='--')
-        plt.title('Function and its Derivative')
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-
-# Пример использования
-if __name__ == "__main__":
-    exp_func = ExponentialFunction(a=2)
-    print(exp_func(0))           # 2.0
-    print(exp_func.derivative(0)) # 2.0
-    
-    exp_func.plot()
